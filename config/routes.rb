4373b7d116031ce61f1841c9ce9d0430eaa17d01
thaynsworth <thomas.haynsworth@gmail.com>
2015-05-19 10:15:01 -0400

    added events section

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   get 'donate' => 'static_pages#donate'
   get 'about' => 'static_pages#about'
+  get 'events' => 'static_pages#events'
   get 'contact' => 'static_pages#contact'
   get 'hcl' => 'static_pages#hcl'
 
