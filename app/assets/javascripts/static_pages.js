b94b048915a86c9586d6bfde1ea36f2a13c779fd
thaynsworth <thomas.haynsworth@gmail.com>
2015-06-11 13:04:14 -0400

    refactored collage code

--- app/assets/javascripts/static_pages.js
+++ app/assets/javascripts/static_pages.js
@@ -1,3 +1,18 @@
 
-
-
+var resizeTimer = null;
+$(window).bind('resize', function() {
+    
+    $('.Collage .Image_Wrapper').css("opacity", 0);
+   
+    if (resizeTimer) clearTimeout(resizeTimer);
+    resizeTimer = setTimeout(collage, 200);
+});
+    
+function collage() {
+    $('.Collage').collagePlus(
+        {
+            'fadeSpeed'     : 2000,
+            'targetHeight'  : 200
+        }
+    );
+};
