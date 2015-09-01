e667ba9134ae269e9c94faba1e7c519000ef90a5
thaynsworth <thomas.haynsworth@gmail.com>
2015-09-01 14:48:33 -0400

    added lightbox

--- Gemfile
+++ Gemfile
@@ -55,3 +55,5 @@ gem 'bootstrap-sass', '~> 3.2.0'
 
 gem 'autoprefixer-rails'
 
+gem 'lightbox2-rails'
+
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,6 +15,7 @@
 //= require turbolinks
 //= require bootstrap-sprockets
 //= require jquery.collagePlus
+//= require lightbox
 //= require_tree .
 
 
